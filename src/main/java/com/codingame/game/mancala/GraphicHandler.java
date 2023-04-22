2cd6f6761cd06a3b11cc18bbbc9da8bcc59a0127
Tanvir Ahmed <tanvir66288@gmail.com>
2023-04-22 17:18:13 +0600

    Initialized GraphicHandler inside constructor

--- src/main/java/com/codingame/game/mancala/GraphicHandler.java
+++ src/main/java/com/codingame/game/mancala/GraphicHandler.java
@@ -1,2 +1,11 @@
-package com.codingame.game.mancala;public class GraphicHandler {
+    public GraphicHandler(GraphicEntityModule graphicEntityModule){
+        this.graphicEntityModule = graphicEntityModule;
+        cups = new Sprite[12];
+        no_plates = new Sprite[12];
+        pots = new Sprite[2];
+        pot_no_plates = new Sprite[2];
+
+        drawBackgroundImage();
+        drawBoardElements();
+    }
 }
