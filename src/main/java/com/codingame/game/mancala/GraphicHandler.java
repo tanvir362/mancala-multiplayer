3d9a881570d9d72da0f00ff083c19b3fc17de7af
Tanvir Ahmed <tanvir66288@gmail.com>
2023-04-22 17:21:48 +0600

    Implemented drawBackground

--- src/main/java/com/codingame/game/mancala/GraphicHandler.java
+++ src/main/java/com/codingame/game/mancala/GraphicHandler.java
@@ -1,3 +1,11 @@
+package com.codingame.game.mancala;
+
+import com.codingame.gameengine.module.entities.GraphicEntityModule;
+import com.codingame.gameengine.module.entities.Sprite;
+
+public class GraphicHandler {
+    private GraphicEntityModule graphicEntityModule;
+
     public GraphicHandler(GraphicEntityModule graphicEntityModule){
         this.graphicEntityModule = graphicEntityModule;
         cups = new Sprite[12];
@@ -5,7 +13,16 @@
         pots = new Sprite[2];
         pot_no_plates = new Sprite[2];
 
-        drawBackgroundImage();
+        drawBackground();
         drawBoardElements();
+    }
+    public void drawBackground(){
+        graphicEntityModule.createSprite()
+                .setImage("background.jpg")
+                .setScaleX(1.5)
+                .setScaleY(1.35)
+                .setAnchor(0)
+                .setX(0)
+                .setY(0);
     }
 }
